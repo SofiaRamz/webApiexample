148754e0b46cb7f49662a491a629264fa0a48f6b
SofiaRamz <l23940403@tlajomulco.tecnm.mx>
2025-05-08 07:31:12 -0600

    Primera version corriendo

---
+++ routes.js
@@ -0,0 +1,36 @@
+const express = require('express')
+
+function escucha(app){
+    const router = express.Router()
+    app.use('/',router)
+
+    router.get('/data', async function(req, res, next){
+        console.log('recibio peticion get en /data')
+        console.log('parametros: ',req.params)
+        console.log('query',req.query)
+        try{
+            res.status(200).json({
+                'nombre':'Sofía',
+                'email':'l23940403@Tlajomulco.tecnm.mx'
+            })
+        }catch(error){
+            console.log(error)
+        }
+    })
+    router.post('/data', async function(req, res, next){
+        console.log('recibio peticion post en /api/datos ')
+        console.log('parametros: ',req.params)
+        console.log('body: ',req.body)
+        console.log('query',req.query)
+        try{
+            res.status(200).json({
+                'Peticion post ':'Satisfactoria',
+                'insertado':"user"
+            })
+        }catch(error){
+            console.log(error)
+        }
+    })
+}
+
+module.exports = escucha
